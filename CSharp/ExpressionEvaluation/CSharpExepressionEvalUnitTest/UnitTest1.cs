6d9a71c70340fa08945fecc1004a294e74d4e5dc
William <crazycoldking@163.com>
2017-02-05 00:45:49 +0800

    Add new solution expression evaluation

---
+++ CSharp/ExpressionEvaluation/CSharpExepressionEvalUnitTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpExepressionEvalUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCalculation()
+        {
+            CSharpExpressionEvaluator.CSharpExpEval eval = new CSharpExpressionEvaluator.CSharpExpEval();
+            var expression = "1+1*1/1";
+            string actual = eval.Execute(expression).ToString();
+            string expect = "2";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestGetDateTime()
+        {
+            CSharpExpressionEvaluator.CSharpExpEval eval = new CSharpExpressionEvaluator.CSharpExpEval();
+            var expression = "DateTime.Now.ToString(\"yyyy-mm-dd\")";
+            string actual = eval.Execute(expression).ToString();
+            string expect = DateTime.Now.ToString("yyyy-mm-dd");
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestRegularExpression()
+        {
+            //new Regex("").IsMatch("");
+            CSharpExpressionEvaluator.CSharpExpEval eval = new CSharpExpressionEvaluator.CSharpExpEval();
+            var expression = "new Regex(\"^(W.?)\").IsMatch(\"Welcome\")";
+            string actual = eval.Execute(expression).ToString();
+            string expect = "True";
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
---
+++ CSharp/ExpressionEvaluation/CSharpExpressionEvaluator/CSharpExpEval.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.CSharp;
+
+
+namespace CSharpExpressionEvaluator
+{
+    //Using attributes below to make the class exposed to COM
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    public class CSharpExpEval
+    {
+        static string Expression_error = "~Expression~Error~";
+        static string Compile_error = "~Compile~Error~";
+        static string Execute_error = "~Execute~Error~";
+
+        /// <summary>
+        /// Evaluate legal C# expression
+        /// </summary>
+        /// <param name="expession">expression</param>
+        /// <returns>return result object</returns>
+        public object Execute(string expession)
+        {
+            string classBody;
+            if (expession.Contains("return") && expession.Contains(";"))
+            {
+                classBody =
+                    "class Exp { delegate object Func(); public static object exec() { try { Func f = delegate() { " +
+                    expession +
+                    "}; return f.Invoke(); } catch { return \"" + Expression_error + "\"; } } }";
+            }
+            else
+            {
+                classBody = "class Exp { public static object exec() { try { return " +
+                            expession +
+                            ";} catch { return \"" + Expression_error + "\"; } } }";
+            }
+
+            //Shell of C# program
+            string moduleSource =
+                "using System;" +
+                "using System.Text;" +
+                "using System.Text.RegularExpressions;" +
+                "namespace eval" +
+                "{" +
+                classBody +
+                "}";
+
+            try
+            {
+                using (CSharpCodeProvider codeProvider = new CSharpCodeProvider())
+                {
+                    CompilerParameters param = new CompilerParameters
+                    {
+                        GenerateExecutable = false,
+                        GenerateInMemory = true
+                    };
+
+                    foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+                    {
+                        try
+                        {
+                            param.ReferencedAssemblies.Add(assembly.Location);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+
+                    CompilerResults compileResult = codeProvider.CompileAssemblyFromSource(param, moduleSource);
+
+                    return GetExecutionResult(compileResult);
+                }
+            }
+            catch (Exception)
+            {
+                return Compile_error;
+            }
+
+        }
+
+        private object GetExecutionResult(CompilerResults compileResult)
+        {
+            object executionResult;
+            if (compileResult.Errors.Count > 0)
+            {
+                executionResult = Execute_error;
+            }
+            else
+            {
+                try
+                {
+                    MethodInfo info = compileResult.CompiledAssembly.GetType("eval.Exp").GetMethod("exec");
+                    executionResult = info.Invoke(null, null);
+                }
+                catch (Exception ex)
+                {
+                    executionResult = Execute_error;
+                }
+            }
+            return executionResult;
+        }
+    }
+}
